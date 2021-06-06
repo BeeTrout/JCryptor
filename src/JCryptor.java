b25990c66a64ad423fc69898961a24e3eb6de648
BeeTrout <tomik9737@gmail.com>
2021-06-06 10:39:39 +0200

    Made the encryption faster

--- src/JCryptor.java
+++ src/JCryptor.java
@@ -3,11 +3,12 @@ public class JCryptor {
     public static void encrypt(byte[] arr) {
 
         int l = arr.length;
+        int q = (int) Math.sqrt(Main.key);
 
         for (int i = 0; i < l; i++) {
 
             if (Main.key % ((i == 0) ? i + 1 : i) == 0) {
-                arr[i] += Main.key / l + i + Math.sqrt(Main.key);
+                arr[i] += Main.key / l + i + q;
             } else {
                 arr[i] += Main.key / l + i;
             }
@@ -19,11 +20,12 @@ public class JCryptor {
     public static void decrypt(byte[] arr) {
 
         int l = arr.length;
+        int q = (int) Math.sqrt(Main.key);
 
         for (int i = 0; i < l; i++) {
 
             if (Main.key % ((i == 0) ? 1 : i) == 0) {
-                arr[i] -= Main.key / l + i + Math.sqrt(Main.key);
+                arr[i] -= Main.key / l + i + q;
             } else {
                 arr[i] -= Main.key / l + i;
             }
